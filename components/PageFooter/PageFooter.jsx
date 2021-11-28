4dc154f14d82fe3c0f950e79355ca40cc2c13400
R. Gonzalo G. Dominguez <gd329525@gmail.com>
2021-11-28 13:52:41 -0300

    primer push

---
+++ components/PageFooter/PageFooter.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Layout} from 'antd';
+
+const { Footer } = Layout;
+
+export default function PageFooter() {
+  return (
+    <div>
+      <Layout>
+        <Footer>footer</Footer>
+      </Layout>
+    </div>
+  );
+}
