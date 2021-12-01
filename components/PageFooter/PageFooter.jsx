662c34a64f071638f2f10f0799e604a970ad6258
R. Gonzalo G. Dominguez <gd329525@gmail.com>
2021-12-01 00:07:27 -0300

    Footer - 1.0.0
    Agregué barra superior y importé col y row de antd.
    
    Generé 4 columnas de elementos y cada una con un titulo y tres parrafos.
    
    Añadí un parrafo con Copyright para un futuro.
    
    generé codigo en el archivo scss.
    
    A HACER:
    Quiero añadir una animación para los distintos elementos al seleccionarlos. Es decir que al pararnos encima de uno de los parrafos del footer se genere una animación similar a la del titulo de la pagina.

--- components/PageFooter/PageFooter.jsx
+++ components/PageFooter/PageFooter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Layout} from 'antd';
+import { Layout, Row, Col } from "antd";
 
 const { Footer } = Layout;
 
@@ -7,7 +7,37 @@ export default function PageFooter() {
   return (
     <>
       <Layout>
-        <Footer className="footer">footer</Footer>
+        <Footer className="footer">
+          <div className="simpleLine"></div>
+          <Row className="mainRow">
+            <Col span={6}>
+              <h4>Acerca de</h4>
+              <p>Terminos y condiciones</p>
+              <p>Quienes somos</p>
+              <p>Resumen</p>
+            </Col>
+            <Col span={6}>
+              <h4>Ayuda</h4>
+              <p>Vender</p>
+              <p>Resolución de problemas</p>
+              <p>Centro de seguridad</p>
+            </Col>
+            <Col span={6}>
+              <h4>Redes sociales</h4>
+              <p>Twitter</p>
+              <p>Facebook</p>
+              <p>YouTube</p>
+            </Col>
+            <Col span={6}>
+              <h4>Mi cuenta</h4>
+              <p>Mis Compras</p>
+              <p>Favoritos</p>
+              <p>Vender</p>
+            </Col>
+          </Row>
+          <br />
+          <p>Copyright © 2021 Uruguay</p>
+        </Footer>
       </Layout>
     </>
   );
